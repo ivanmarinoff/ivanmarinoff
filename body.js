b85184c154529ae56cfc6e46bcbba3804268b4e2
ivanmarinoff <marinoffivan@gmail.com>
2024-03-16 15:43:34 +0200

    Updated

--- body.js
+++ body.js
@@ -5,4 +5,3 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
 `;
 
-// You can then use googleTagManagerNoScript as needed in your tagscript.js file
